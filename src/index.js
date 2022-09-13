94a23a0a138612515aab6f7cde3ee65abc56ec0d
MiroProger <sirmiroslavsuprun@gmail.com>
2022-09-13 19:20:55 +0300

    Practising with webpack, added some packages and loaders

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./css/styles.css";
+console.log("hlos");
+
+if (amount >= 10 && amoun <= 200) {
+  if (amount % 10 === 0) {
+    return;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+  },
+};
